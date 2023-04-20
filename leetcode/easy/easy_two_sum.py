c3be0ac9c62a53a335f6cda363d9ccf0aa72e6cc
bnsbns <y3kmarvin@gmail.com>
2023-04-21 06:37:55 +0800

    paused

--- leetcode/easy/easy_two_sum.py
+++ leetcode/easy/easy_two_sum.py
@@ -5,5 +5,31 @@ You may assume that each input would have exactly one solution, and you may not 
 
 You can return the answer in any order.
 
+Eg. 1
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+
+Eg. 2
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Eg. 3
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
 """
 
+from typing import List
+import logging
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        print(nums)
+        sorted_nums = nums
+        sorted_nums.sort()
+        # logging.info(sorted_nums)
+        print(f'sorted nums: {sorted_nums}')
+        return sorted_nums
